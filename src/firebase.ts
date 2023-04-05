365860d0dd57b7f6516e364e7404670f9751e4df
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-05 16:11:10 +0900

    [Feat] title, 아이콘 수정

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCM5qrO5-VyokbcIaPArVK-QqLZIcaRfMs",
+  authDomain: "shoppy-5220c.firebaseapp.com",
+  projectId: "shoppy-5220c",
+  storageBucket: "shoppy-5220c.appspot.com",
+  messagingSenderId: "688941284499",
+  appId: "1:688941284499:web:a8c93557f843e13d322166",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
