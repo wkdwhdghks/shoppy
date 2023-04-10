34b1abc403d989ac6fffb7c8ecbd4f9df5cafdca
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-04-11 01:42:32 +0900

    [Style] 장바구니 ui 스타일링

--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -5,5 +5,12 @@ export default function PriceCard({
   text: string;
   price: number;
 }): JSX.Element {
-  return <div></div>;
+  return (
+    <div className="bg-gray-50 p-8 mx-2 rounded-2xl text-center text-lg md:text-xl">
+      <p>{text}</p>
+      <p className="font-bold text-brand text-xl md:text-2xl">
+        ₩{price.toLocaleString()}
+      </p>
+    </div>
+  );
 }
--- src/pages/MyCart.tsx
+++ src/pages/MyCart.tsx
@@ -5,6 +5,7 @@ import CartItem from "../components/ui/CartItem";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { FaEquals } from "react-icons/fa";
 import PriceCard from "../components/PriceCard";
+import Button from "../components/ui/Button";
 
 const SHIPPING: number = 3000;
 
@@ -26,32 +27,36 @@ export default function MyCart(): JSX.Element {
     products &&
     products.reduce(
       (prev: any, current: any) =>
-        prev + parseInt(current.price) * current.quantitiy,
+        prev + parseInt(current.price) * current.quantity,
       0
     );
 
   return (
-    <section>
-      <p>내 장바구니</p>
+    <section className="p-8">
+      <p className="text-2xl text-center font-bold pb-4 border-b border-gray-300">
+        내 장바구니
+      </p>
       {!hasProducts && <p>장바구니에 상품이 없습니다. 열심히 쇼핑해 주세요!</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className="border-b border-gray-300 mb-8 p-4 px-8">
             {products &&
               products.map((product: any) => (
                 <CartItem key={product.id} product={product} uid={user.uid} />
               ))}
           </ul>
-          <div>
-            <PriceCard text="상품 총액" price={totalPrice as number} />
-            <BsFillPlusCircleFill />
+          <div className="flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16">
+            {typeof totalPrice === "number" && (
+              <PriceCard text="상품 총액" price={totalPrice} />
+            )}
+            <BsFillPlusCircleFill className="shrink-0" />
             <PriceCard text="배송액" price={SHIPPING} />
-            <FaEquals />
-            <PriceCard
-              text="총가격"
-              price={(totalPrice as number) + SHIPPING}
-            />
+            <FaEquals className="shrink-0" />
+            {typeof totalPrice === "number" && (
+              <PriceCard text="총가격" price={totalPrice + SHIPPING} />
+            )}
           </div>
+          <Button text="주문하기" onClick={undefined} />
         </>
       )}
     </section>
